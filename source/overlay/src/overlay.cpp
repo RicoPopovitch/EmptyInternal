b97c15db390359754268a610f2494218ed07618e
Don Luciano <ivyurbad@gmail.com>
2025-08-28 19:12:08 -0700

    added imgui

--- source/overlay/src/overlay.cpp
+++ source/overlay/src/overlay.cpp
@@ -1,16 +1,97 @@
+#include <d3d11.h>
+#include "../../vendor/imgui/imgui.h"
+#include "../../vendor/imgui/backends/imgui_impl_win32.h"
+#include "../../vendor/imgui/backends/imgui_impl_dx11.h"
+
+#pragma comment(lib, "d3d11.lib")
+
+static ID3D11Device*           g_pd3dDevice = nullptr;
+static ID3D11DeviceContext*    g_pd3dDeviceContext = nullptr;
+static IDXGISwapChain*         g_pSwapChain = nullptr;
+static ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
+
+void CreateRenderTarget()
+{
+    ID3D11Texture2D* pBackBuffer = nullptr;
+    g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
+    pBackBuffer->Release();
+}
+
+void CleanupRenderTarget()
+{
+    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
+}
+
+bool CreateDeviceD3D(HWND hWnd)
+{
+    DXGI_SWAP_CHAIN_DESC sd = {};
+    sd.BufferCount = 2;
+    sd.BufferDesc.Width = 0;
+    sd.BufferDesc.Height = 0;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+    UINT createDeviceFlags = 0;
+    D3D_FEATURE_LEVEL featureLevel;
+    const D3D_FEATURE_LEVEL featureLevelArray[1] = { D3D_FEATURE_LEVEL_11_0 };
+    if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 1,
+        D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
+        return false;
+    CreateRenderTarget();
+    return true;
+}
+
+void CleanupDeviceD3D()
+{
+    CleanupRenderTarget();
+    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = nullptr; }
+    if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = nullptr; }
+    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
+}
+
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+#include <windows.h>
 #include "overlay.h"
 
+#ifndef GET_X_LPARAM
+#define GET_X_LPARAM(lp) ((int)(short)LOWORD(lp))
+#define GET_Y_LPARAM(lp) ((int)(short)HIWORD(lp))
+#endif
+
+static bool g_clickThrough = false;
+static bool g_menuVisible = true;
+static HWND g_overlayHwnd = nullptr;
+
 LRESULT CALLBACK OverlayWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    if (msg == WM_PAINT) {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-        SetBkMode(hdc, TRANSPARENT);
-        SetTextColor(hdc, RGB(255, 0, 0));
-        TextOutA(hdc, 50, 50, "Test", 15);
-        EndPaint(hwnd, &ps);
+    if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wParam, lParam))
+        return true;
+    switch (msg) {
+    case WM_SIZE:
+        if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+        {
+            CleanupRenderTarget();
+            g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+            CreateRenderTarget();
+        }
         return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU)
+            return 0;
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    default:
+        return DefWindowProcA(hwnd, msg, wParam, lParam);
     }
-    return DefWindowProcA(hwnd, msg, wParam, lParam);
 }
 
 DWORD WINAPI OverlayThread(LPVOID) {
@@ -19,22 +100,84 @@ DWORD WINAPI OverlayThread(LPVOID) {
     wc.hInstance = GetModuleHandleA(NULL);
     wc.lpszClassName = "OverlayWindowClass";
     wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     RegisterClassA(&wc);
 
     HWND hwnd = CreateWindowExA(
-        WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST,
+        WS_EX_LAYERED | WS_EX_TOPMOST,
         "OverlayWindowClass", NULL, WS_POPUP,
         0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
         NULL, NULL, wc.hInstance, NULL);
 
+    g_overlayHwnd = hwnd;
     SetLayeredWindowAttributes(hwnd, RGB(0,0,0), 0, LWA_COLORKEY);
     ShowWindow(hwnd, SW_SHOW);
 
-    MSG msg;
-    while (GetMessageA(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessageA(&msg);
+    if (!CreateDeviceD3D(hwnd)) {
+        CleanupDeviceD3D();
+        return 1;
     }
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+
+    MSG msg;
+    bool done = false;
+    while (!done) {
+        if (GetAsyncKeyState(VK_INSERT) & 1) {
+            g_clickThrough = !g_clickThrough;
+            LONG exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            if (g_clickThrough) {
+                SetWindowLong(hwnd, GWL_EXSTYLE, exStyle | WS_EX_TRANSPARENT);
+            } else {
+                SetWindowLong(hwnd, GWL_EXSTYLE, exStyle & ~WS_EX_TRANSPARENT);
+            }
+        }
+        if (GetAsyncKeyState(VK_DELETE) & 1) {
+            g_menuVisible = !g_menuVisible;
+        }
+        if ((GetAsyncKeyState(VK_LSHIFT) & 0x8000) && (GetAsyncKeyState(VK_F12) & 1)) {
+            DestroyWindow(hwnd);
+            break;
+        }
+        while (PeekMessageA(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_QUIT) done = true;
+            TranslateMessage(&msg);
+            DispatchMessageA(&msg);
+        }
+        ImGui_ImplDX11_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+        ImGui::NewFrame();
+        static bool demoCheckbox = false;
+        static bool demoCheckbox2 = false;
+        static char demoInput[128] = "";
+        static float demoSlider = 0.0f;
+        static bool demoButtonPressed = false;
+        if (g_menuVisible) {
+            ImGui::SetNextWindowSize(ImVec2(300, 130));
+            ImGui::Begin("Overlay");
+            ImGui::Checkbox("##Checkbox", &demoCheckbox);
+            ImGui::Checkbox("##Checkbox2", &demoCheckbox2);
+            ImGui::PushItemWidth(-1);
+            ImGui::InputText("##Input", demoInput, IM_ARRAYSIZE(demoInput));
+            ImGui::SliderFloat("##Slider", &demoSlider, 0.0f, 100.0f);
+            ImGui::PopItemWidth();
+            ImGui::End();
+        }
+        ImGui::Render();
+        const float clear_color[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+        g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+        g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, clear_color);
+        ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+        g_pSwapChain->Present(1, 0);
+        Sleep(10);
+    }
+    ImGui_ImplDX11_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+    CleanupDeviceD3D();
     return 0;
 }
 
